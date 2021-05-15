ebf5370ae29abf84358c5a5a44e059158c955a75
Issac Antony <issacantony123@gmail.com>
2021-05-15 19:07:45 +0530

    Updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p-(g!=l)%_)3lkd2ygc5zt!tcl23#s(y(1goh-90ckw@5nuvff'
+# SECRET_KEY = 'p-(g!=l)%_)3lkd2ygc5zt!tcl23#s(y(1goh-90ckw@5nuvff'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['issacdjangoapp.herokuapp.com']
 
 
 # Application definition
@@ -137,8 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('DJANGO_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_PASS')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
