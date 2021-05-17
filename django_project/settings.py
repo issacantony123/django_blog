f47961572ca71b606e6dd85ffab0acb7c9881e7e
Issac Antony <issacantony123@gmail.com>
2021-05-17 10:50:13 +0530

    removed sensitive info

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'p-(g!=l)%_)3lkd2ygc5zt!tcl23#s(y(1goh-90ckw@5nuvff'
+# SECRET_KEY =
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
